811fe8c5c8a797eb50f6a604d4b090b4b189c25d
somratcste <somratcste@gmail.com>
2016-07-08 08:54:55 +0600

    the main view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.master')
+
+@section('title')
+	Trending Quotes
+@endsection
 @section('content')	
-	@if(count($errors) > 0)
+	{{-- @if(count($errors) > 0)
 		<div>
 			<ul class="alert alert-danger">
 				@foreach ($errors->all() as $error)
@@ -8,9 +12,32 @@
 				@endforeach
 			</ul>
 		</div>
-	@endif
+	@endif --}}
+	
+	<section class="quotes">
+		<h1>Latest Quotes</h1>
+		<article class="quote">
+			<div class="delete"><a href="#">X</a></div>
+			Quote Text
+			<div class="info">Created by <a href="#">Somrat </a>On ...</div>
+		</article>
+		Pagination
+	</section>
 
-	<div class="row">
+	<section class="edit-quote">
+		<h1>Add a Quote</h1>
+		<form>
+			<div class="input-group">
+				<label for="author">Your Name : </label>
+				<input type="text" name="author" id="author" placeholder="your name">
+			</div>
+			<div class="input-group">
+				<label for="quote">Your Quote : </label>
+				<textarea rows="5" type="text" name="quote" id="quote" placeholder="your quote"></textarea>
+			</div>
+			<button type="button" class="btn btn-primary">Submti Quote</button>
+			<input type="hidden" name="_token" value="{{Session::token()}}">
+		</form>
+	</section>
 
-	</div>
 @endsection
