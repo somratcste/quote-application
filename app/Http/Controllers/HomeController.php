f2b0ab33f1f7fa639f6671ca4a9dfa63b0046b5b
somratcste <somratcste@gmail.com>
2016-07-09 21:08:00 +0600

    Display author name

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,27 @@
 namespace App\Http\Controllers;
 use App\Quote;
 use App\Author;
+use DB;
 
 class HomeController extends Controller 
 {
 	public function getHome($author = null)
 	{
-		if(!is_null($author))
-		{
-			$quote_author = Author::where('name' , $author)->first();
-			if($quote_author)
-			{
-				$quotes = $quote_author->quotes()->orderBy('created_at' , 'desc')-paginate(6);
-			}
-		} else {
-			$quotes = Quote::orderBy('created_at' , 'desc')->paginate(3);
-		}		
+		// if(!is_null($author))
+		// {
+		// 	$quote_author = Author::where('name' , $author)->first();
+		// 	if($quote_author)
+		// 	{
+		// 		$quotes = $quote_author->quotes()->orderBy('created_at' , 'desc')-paginate(6);
+		// 	}
+		// } else {
+		// 	
+		// }	
+		$quotes = DB::table('quotes')
+					->join('authors' , 'quotes.author_id' , '=' , 'authors.id')
+					->select('quotes.*' , 'authors.name')
+		            ->paginate(6);	
+
 		return view('home', ['quotes' => $quotes]);
 	}
 	
